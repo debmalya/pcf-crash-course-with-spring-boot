3aa2d5012593695e2e757752ca19937cb86d1b4c
deb malya <debmalya.jash@thalesgroup.com>
2020-09-12 16:09:26 +0800

    Quotes

---
+++ 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/QuoteController.java
@@ -0,0 +1,16 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.deb.quote.Quote;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuoteController {
+
+	@GetMapping("/quote")
+	public String getQuote(){
+		return new Quote().getQuote();
+	}
+	
+}
